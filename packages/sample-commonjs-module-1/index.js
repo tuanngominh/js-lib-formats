b458f769560761e433cc65985960d606d7f6176f
Tuan Ngo <tuannm79@gmail.com>
2021-09-04 14:35:22 +0700

    chore

---
+++ packages/sample-commonjs-module-1/index.js
@@ -0,0 +1,12 @@
+function square(x) {
+    return x * x;
+}
+
+function squareRoot(x) {
+    return Math.sqrt(x);
+}
+
+// some comments to trigger version change
+
+exports.square = square;
+exports.squareRoot = squareRoot;
